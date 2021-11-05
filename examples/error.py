542d91fdc25f39b46f06575f3d1af4e26c4f6c81
Quinn Dougherty <quinn.dougherty92@gmail.com>
2021-11-04 23:41:20 -0400

    stuff a python linter would tell you

--- examples/error.py
+++ examples/error.py
@@ -1,6 +1,9 @@
-import pycoq, coq
+import pycoq
+import coq
 
-# parsing error
-opts = {"newtip": None, "ontop": None, "lim": 10 }
-res = coq.add("Definition a := .", opts)
-print(res)
+if __name__=="__main__":
+
+    # parsing error
+    opts = {"newtip": None, "ontop": None, "lim": 10 }
+    res = coq.add("Definition a := .", opts)
+    print(res)
